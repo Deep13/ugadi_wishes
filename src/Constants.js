addf509d3e7e9343a7710e779cccbbdcdd7d5c26
Deep13 <deep.deepak30@gmail.com>
2024-05-19 13:22:15 +0530

    initial

---
+++ src/Constants.js
@@ -0,0 +1,101 @@
+export const day_title = 'Happy Ugadi';
+export const banner_key1 = 'ca-app-pub-4717579333914549/7091987340';
+export const banner_key2 = 'ca-app-pub-4717579333914549/4358298204';
+export const banner_key3 = 'ca-app-pub-4717579333914549/9419053199';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/5479808183';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/7914399833';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/5288236494';
+export const language = ['English', 'Telugu'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'Put all the shadows of the past behind Let’s look forward to new start Realize new aspirations and dreams Find new courage and hope to achieve them. Wishing you an Ugadi filled with happiness and fun!',
+      },
+      {
+        id: '002',
+        text:
+          'May you blessed this Ugadi. May the New Year bring you a life that is Successful, abundant, prosperous and contenting. Have a happy Ugadi!',
+      },
+      {
+        id: '003',
+        text:
+          'Welcome the New Year with complete anticipation, hope and eagerness. Look forward to a bountiful year of prosperity, satisfaction and peace. Have a very happy Ugadi!',
+      },
+      {
+        id: '004',
+        text:
+          'This Ugadi, I’m wishing you a life filled with peace, cheer and laughter. I pray that you gain might and strength to accept life’s ups and downs. Happy Ugadi to you!',
+      },
+      {
+        id: '005',
+        text:
+          'Celebrate Ugadi with complete cheer and joy, Together with people you love and love you back Have a happy Ugadi!',
+      },
+      {
+        id: '006',
+        text:
+          'This Ugadi, I wish you to have a new year filled with laughter, joy and fulfillment. May the best returns of life be with you. Have a blessed Ugadi!',
+      },
+      {
+        id: '007',
+        text:
+          'Best wishes to you and your family this Ugadi Festival. Have lots of fun and bond! Let the Ugadi Festival usher in joyous and great New Year Happy Ugadi to you and to your family!',
+      },
+      {
+        id: '008',
+        text:
+          'Wishing you a LOVELY Ugadi Lively Optimal Vibrant Exciting Likeable Yielding Happy Ugadi To you!',
+      },
+      {
+        id: '009',
+        text:
+          'May Ugadi festival be the start of your successful and prosperous life. I wish you all the best this year! Wishing you a very Happy Ugadi!',
+      },
+      {
+        id: '010',
+        text:
+          "Let's welcome Ugadi with great hope, eagerness & anticipation. Let us look forward to a plentiful of joy, satisfaction, peace & prosperity. Happy Ugadi!",
+      },
+    ]
+  }
+  ,
+  Telugu: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'మీకు మరియు మీ కుటుంబ సభ్యులకు ఉగాడి శుభ సందర్భంగా చాలా నూతన సంవత్సర శుభాకాంక్షలు.',
+      },
+      {
+        id: '002',
+        text:
+          'మీరు నూతన నూతన సంవత్సరాన్ని స్వాగతించి ఆనందం మరియు శ్రేయస్సు యొక్క మార్గంలో బయలుదేరినప్పుడు మీ మార్గం ఆనందకరమైన ఉత్సవాలను కోరుకుంటున్నాను. హ్యాపీ ఉగాది!',
+      },
+      {
+        id: '003',
+        text:
+          'నూతన సంవత్సరం మీకు జీవితాన్ని తెస్తుంది, అది విజయవంతమైనది, సమృద్ధిగా, సంపన్నమైనది మరియు సంతృప్తికరంగా ఉంటుంది. హ్యాపీ ఉగాది!',
+      },
+      {
+        id: '004',
+        text:
+          'ఈ అందం పండుగను ఆశించడం వల్ల మీ మార్గం, సంతృప్తికరమైన ప్రకాశవంతమైన మరుపులు, రాబోయే రోజుల్లో మీతోనే ఉంటాయి. ఉగాడి మరియు నూతన సంవత్సరానికి శుభాకాంక్షలు.',
+      },
+      {
+        id: '005',
+        text:
+          'హృదయం నుండి నేరుగా వచ్చేంతవరకు ఏ కోరిక చిన్నది లేదా పెద్దది కాదు. ఈ ఉగాడి కోరిక మిమ్మల్ని మంచి ఆత్మతో కనుగొంటుందని ఆశిస్తున్నాను.',
+      },
+      {
+        id: '006',
+        text:
+          'ఈ ఉగాది ఎప్పటిలాగే ప్రకాశవంతంగా ఉండనివ్వండి ఈ ఉగాది మీకు ఆనందం, ఆరోగ్యం మరియు సంపదను తెస్తుంది లైట్ల పండుగ మిమ్మల్ని మరియు మీ సమీప మరియు ప్రియమైనవారి జీవితాలను ప్రకాశవంతం చేస్తుంది హ్యాపీ ఉగాడి',
+      },
+    ]
+  }
+
+
+};
